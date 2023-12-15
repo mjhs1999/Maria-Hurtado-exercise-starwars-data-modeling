8653cb5e833213538806423a988b7ba0cda2d166
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-12-15 19:13:23 +0000

    Creating user and ship

--- src/models.py
+++ src/models.py
@@ -5,28 +5,27 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
-Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    email = Column(Integer, primary_key=True)
+    usuario = Column(String(250), ForeignKey('naves_favoritas'),ForeignKey('personas_favoritas'),ForeignKey('planetas_favoritas'))
+    password = Column(String(250), nullable=False)
+## creating ship class
+class Naves(Base):
+    __tablename__ = 'naves'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    modelo = Column(String(250), nullable=False)
+    clase_de_nave = Column(String(250), nullable=False)
+    costo_en_creditos = Column(String(250), nullable=False)
+    longitud = Column(String(250), nullable=False)
+    capacidad_de_pasajeros = Column(Integer, nullable=False)
+    MGLT = Column(String(250), nullable=False)
+    capacidad_de_carga = Column(Integer, nullable=False)
+    provisiones = Column(Integer, nullable=False)
+    hypermotor = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
-## Draw from SQLAlchemy base
+    ##creating person class
+    ##creating planets
+    ##creating favs
 render_er(Base, 'diagram.png')
